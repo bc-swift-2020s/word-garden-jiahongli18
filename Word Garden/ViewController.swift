510a85631b75ec021678d02195f4868f105cafa8
Jiahong Li <libya@bc.edu>
2020-02-02 23:06:10 -0500

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Jia H Li on 2/2/20.
+//  Copyright © 2020 Jia H Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
